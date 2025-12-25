6b074f7b133836f71a1348213fdae05ca093c3e0
Dom Rodriguez <shymega@shymega.org.uk>
2025-12-25 21:20:27 +0000

    refactor: Rename to Wand Launcher

--- wemod.bat
+++ wemod.bat
@@ -1,16 +1,16 @@
 @echo off
-@title Wemod Launcher
+@title Wand Launcher
 
 SET mypath=%~dp0
-SET wemodname=WeMod.exe
-SET wemodpath=%mypath:~0,-1%\wemod_bin\%wemodname%
+SET wandname=Wand.exe
+SET wandpath=%mypath:~0,-1%\wand_bin\%wandname%
 SET temptime=%mypath:~0,-1%\.cache\early.tmp
 SET returnfile=%mypath:~0,-1%\.cache\return.tmp
 
-echo Hello from the WeMod Launcher, the WeMod bat was started successfully.
+echo Hello from the Wand Launcher, the Wand bat was started successfully.
 echo.
-echo WEMOD EXE:
-echo "%wemodpath%"
+echo WAND EXE:
+echo "%wandpath%"
 echo.
 echo PWD:
 echo "%cd%"
@@ -21,56 +21,56 @@ echo %*
 echo.
 echo.
 
-REM Start WeMod.exe and get its PID
-echo Starting WeMod by using %wemodname%.
-start "" %wemodpath%
+REM Start Wand.exe and get its PID
+echo Starting Wand by using %wandname%.
+start "" %wandpath%
 
-echo Checking for running WeMod pid
+echo Checking for running Wand pid
 set retry_count=0
 :retry_pid
-set wemodPID=
+set wandPID=
 set /a retry_count+=1
 
-REM Get the wemod pid over Proton
-if not defined wemodPID (
-    for /F "TOKENS=1,2,*" %%a in ('C:/windows/system32/tasklist /FI "IMAGENAME eq %wemodname%"') do (
+REM Get the wand pid over Proton
+if not defined wandPID (
+    for /F "TOKENS=1,2,*" %%a in ('C:/windows/system32/tasklist /FI "IMAGENAME eq %wandname%"') do (
         set void=%%a
-        set wemodPID=%%b
+        set wandPID=%%b
     )
 )
-REM Retry to get the wemod pid over Proton
-if not defined wemodPID (
-    for /F "TOKENS=1,2,*" %%a in ('C:/windows/system32/tasklist /FI "IMAGENAME eq %wemodname%"') do (
+REM Retry to get the wand pid over Proton
+if not defined wandPID (
+    for /F "TOKENS=1,2,*" %%a in ('C:/windows/system32/tasklist /FI "IMAGENAME eq %wandname%"') do (
         set void=%%a
-        set wemodPID=%%b
+        set wandPID=%%b
     )
 )
-REM If still not set get wemod pid over wine
-if not defined wemodPID (
-    for /F "TOKENS=2 delims=," %%d in ('C:/windows/system32/tasklist /FI "IMAGENAME eq %wemodname%"') do (
-        set wemodPID=%%d
+REM If still not set get wand pid over wine
+if not defined wandPID (
+    for /F "TOKENS=2 delims=," %%d in ('C:/windows/system32/tasklist /FI "IMAGENAME eq %wandname%"') do (
+        set wandPID=%%d
     )
 )
-REM And If still not set retry getting the wemod pid over wine
-if not defined wemodPID (
-    for /F "TOKENS=2 delims=," %%d in ('C:/windows/system32/tasklist /FI "IMAGENAME eq %wemodname%"') do (
-        set wemodPID=%%d
+REM And If still not set retry getting the wand pid over wine
+if not defined wandPID (
+    for /F "TOKENS=2 delims=," %%d in ('C:/windows/system32/tasklist /FI "IMAGENAME eq %wandname%"') do (
+        set wandPID=%%d
     )
 )
 
-if not defined wemodPID (
-    echo Attempting to find WeMod PID (attempt %retry_count% of 3)...
+if not defined wandPID (
+    echo Attempting to find Wand PID (attempt %retry_count% of 3)...
     if %retry_count% leq 3 (
         @ping localhost -n 1 > NUL 2>&1
         goto retry_pid
     )
-    echo Failed to find WeMod PID after multiple attempts. Continuing anyway.
+    echo Failed to find Wand PID after multiple attempts. Continuing anyway.
 )
 
-echo WeMod found with pid %wemodPID%
+echo Wand found with pid %wandPID%
 echo.
 
-echo WeMod found with pid %wemodPID%
+echo Wand found with pid %wandPID%
 echo.
 
 REM Start the game and wait for exit
@@ -80,14 +80,14 @@ start /wait "" %*
 echo.
 echo The game was closed
 
-if defined wemodPID (
+if defined wandPID (
     if exist %temptime% (
         del %temptime%
         echo Game closed to fast, Game detection may have failed. > %returnfile%
-        echo Keep in mind the wemod-launcher usualy can`t detect game launchers. >> %returnfile%
+        echo Keep in mind the wand-launcher usualy can`t detect game launchers. >> %returnfile%
         echo THIS IS NOT A BUG, its not possible with the current project structure. >> %returnfile%
         echo Only open a Issue if the game (launcher) did not start, >> %returnfile%
-        echo or if the game crashed, or if wemod won't closes unexpectedly. >> %returnfile%
+        echo or if the game crashed, or if wand won't closes unexpectedly. >> %returnfile%
         echo.
         echo Game closed to fast, Game detection may have failed, sending problem to python script and waiting for awnser
         :WaitUser
@@ -96,11 +96,11 @@ if defined wemodPID (
             goto WaitUser
         )
     )
-    echo Closing WeMod
-    C:/windows/system32/taskkill.exe /PID %wemodPID% /F 2>NUL
-    C:/windows/system32/taskkill.exe /PID %wemodPID% /F 2>NUL
+    echo Closing Wand
+    C:/windows/system32/taskkill.exe /PID %wandPID% /F 2>NUL
+    C:/windows/system32/taskkill.exe /PID %wandPID% /F 2>NUL
     echo.
-    echo Killed %wemodname% with pid %wemodPID%
+    echo Killed %wandname% with pid %wandPID%
 )
 
 echo.
